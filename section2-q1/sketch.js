7b0088559147ccdb3d7fdfad26f01af466e78e5d
yurika2424 <65202897+yurika2424@users.noreply.github.com>
2020-10-20 13:03:46 +0900

    section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,13 @@
 // 小手調べ
 function setup(){
+    createCanvas(100,100);
   for(let i = 0; i < 10; i++){
-    // BLANK[1]
-  }
+
+if(i<5){  stroke(255,0,0);}
+else{stroke(0,0,255)}
+
+    ellipse(50,50,50-5*i);
+
+
+}
 }
