a2c97a3143446b57cf1d8b478bedddbf1d64456e
yurika2424 <65202897+yurika2424@users.noreply.github.com>
2020-10-27 12:41:17 +0900

    Update sketch.js

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -4,15 +4,30 @@
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46");
+  balloon("I love keyakizaka46", 20, 30);
+  fill(0);
+
+  regularPolygon(3,133,37, 20);
 }
 
-function balloon(t){
+function balloon(t, x, y){
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 2;
+  let p = 9; //余白の大きさ。ここまでの3行が吹き出しの大きさ
   fill(0);
-  rect(0, 0, w + p * 2, h + p * 2);
-  fill(255);
+  rect(0, 0, w + p * 2, h + p * 3);
+  fill(255, 255, 0);
   text(t, p, h + p);
+}
+function regularPolygon(n, cx, cy, r){
+  beginShape();
+  for(var i = 0; i < n; i++){
+    let theta = TWO_PI * i  / n - HALF_PI;
+    let x = cx + cos(theta) * r;
+    let y = cy + sin(theta) * r;
+    vertex(x,y);
+  }
+  endShape(CLOSE);
+
+
 }
