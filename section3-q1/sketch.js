ee2633c000c6820d1903d1112ad3903b0febe181
yurika2424 <65202897+yurika2424@users.noreply.github.com>
2020-11-07 15:49:54 +0900

    Update sketch.js

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,33 +1,33 @@
+
 // 練習問題：吹き出し
 // 吹き出しの位置、背景色 etc. を関数 balloon の引数で指定できるようにしてみよう
 // 吹き出しにしっぽを付けてみよう
 function setup(){
   createCanvas(400, 400);
   background(255);
-  balloon("I love keyakizaka46", 20, 30);
-  fill(0);
-
-  regularPolygon(3,133,37, 20);
+  balloon("I love keyakizaka46");
 }
+
 
 function balloon(t, x, y){
+
   let w = textWidth(t);
   let h = textAscent() + textDescent();
-  let p = 9; //余白の大きさ。ここまでの3行が吹き出しの大きさ
-  fill(0);
-  rect(0, 0, w + p * 2, h + p * 3);
-  fill(255, 255, 0);
-  text(t, p, h + p);
-}
-function regularPolygon(n, cx, cy, r){
-  beginShape();
-  for(var i = 0; i < n; i++){
-    let theta = TWO_PI * i  / n - HALF_PI;
-    let x = cx + cos(theta) * r;
-    let y = cy + sin(theta) * r;
-    vertex(x,y);
-  }
-  endShape(CLOSE);
+  let p = 50;
+  let a = 10;
 
+  fill(52, 168, 50);
+  rect(a, a, w + p * 2, h + p * 2);
+
+  fill(255);
+  textSize(18);
+  text(t, p * 0.5 + a, h + p + a * 0.5);
+
+
+
+  fill(52, 168, 50);
+  triangle((w + p * 2) * 0.8, h + p * 2 + a,
+   (w + p * 2) * 1, h + p * 2 + a,
+   (w + p * 2) * 1.4, (h + p * 2 + a) * 1.3);
 
 }
